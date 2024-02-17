7ffeaf3bb4460f9b22059f84e02e83d6fc37a721
Pavel Mernov <pkmernov@edu.hse.ru>
2024-02-17 18:22:15 +0300

    Started developing a custom search control

--- HandShaker/Assets/UniversalElements/SearchBox.cs
+++ HandShaker/Assets/UniversalElements/SearchBox.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace HandShaker.Assets.UniversalElements
 {
-	internal class SearchBox : TextBox
+	internal class SearchBox : Control
 	{
 		public SearchBox()
 		{
-
+			Style = (Style)FindResource("SearchBoxStyle");
 		}
 	}
 }
